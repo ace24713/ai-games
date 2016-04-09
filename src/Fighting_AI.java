8986033646350eddcd32964ecf3e157c5a9ddc35
Andrew Caputi <acaputi@optonline.net>
2016-04-09 00:02:40 -0400

    Initial commit - getting AI project info online.

---
+++ src/Fighting_AI.java
@@ -0,0 +1,46 @@
+/**
+ * Created by Ace on 4/8/2016.
+ */
+import gameInterface.AIInterface;
+import structs.FrameData;
+import structs.GameData;
+import structs.Key;
+
+public class Fighting_AI implements AIInterface
+{
+    @Override
+    public void close()
+    {
+
+    }
+
+    @Override
+    public String getCharacter()
+    {
+        return null;
+    }
+
+    @Override
+    public void getInformation(FrameData arg0)
+    {
+
+    }
+
+    @Override
+    public int initialize(GameData var1, boolean var2)
+    {
+        return 0;
+    }
+
+    @Override
+    public Key input()
+    {
+        return null;
+    }
+
+    @Override
+    public void processing()
+    {
+
+    }
+}
