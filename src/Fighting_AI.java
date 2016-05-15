9568fbc0beb679cb8674bb3c5359abe872515493
Jonathan <jma533@nyu.edu>
2016-05-15 17:05:28 -0400

    Basic MCTS implementation reusing Japanese code. Translated and made to with our project. Perusing the code and comments would make it fairly easy to understand. There are some efficiency changes that can be made to make it all run smoother look cleaner though.

--- src/Fighting_AI.java
+++ src/Fighting_AI.java
@@ -1,46 +1,241 @@
 /**
- * Created by Ace on 4/8/2016.
+ * AI built using MCTS AI model and code by author Taichi from ICE_Fighting
+ *
+ * Created by Jonathan on 4/8/2016.
  */
+
+import com.sun.org.apache.bcel.internal.generic.NEW;
+import enumerate.Action;
+import enumerate.State;
 import gameInterface.AIInterface;
+
+import java.util.LinkedList;
+import java.util.Vector;
+
+import simulator.Simulator;
+import structs.CharacterData;
 import structs.FrameData;
 import structs.GameData;
 import structs.Key;
+import structs.MotionData;
 
-public class Fighting_AI implements AIInterface
-{
+import commandcenter.CommandCenter;
+
+public class Fighting_AI implements AIInterface {
+
+    private Simulator simulator;
+    private Key key;
+    private boolean playerNumber;
+    private CommandCenter commandCenter;
+    private GameData gameData;
+
+    private FrameData frameData;
+
+    /* Frame data delayed by FRAME_AHEAD */
+    private FrameData simulatorAheadFrameData;
+
+    /* Actions AI can perform */
+    private LinkedList<Action> myActions;
+
+    /* All actions the opponent can perform */
+    private LinkedList<Action> oppActions;
+
+    /* AI character information */
+    private CharacterData myCharacter;
+
+    /* Opponent character information */
+    private CharacterData oppCharacter;
+
+    /* Adjustment frame time delay */
+    private static final int FRAME_AHEAD = 14;
+
+    private Vector<MotionData> myMotion;
+
+    private Vector<MotionData> oppMotion;
+
+    private Action[] actionAir;
+
+    private Action[] actionGround;
+
+    private Action spSkill;
+
+    private Node rootNode;
+
+    /* Debug mode activation boolean. If true, constructs output log. */
+    public static final boolean DEBUG_MODE = false;
+
     @Override
-    public void close()
-    {
+    public void close() { }
 
+    @Override
+    /* This method is for deciding which character to use among ZEN, GARNET, LUD, and KFM,
+     and it returns one of the following values, which must be specified after "return"
+      for the competition: CHARACTER_ZEN, CHARACTER_GARNET, CHARACTER_LUD, and CHARACTER_KFM */
+    public String getCharacter() { return CHARACTER_ZEN; }
+
+    @Override
+    public void getInformation( FrameData frameData ) {
+        this.frameData = frameData;
+        this.commandCenter.setFrameData( this.frameData, playerNumber );
+
+        if (playerNumber) {
+            myCharacter = frameData.getP1();
+            oppCharacter = frameData.getP2();
+        }
+
+        else {
+            myCharacter = frameData.getP2();
+            oppCharacter = frameData.getP1();
+        }
     }
 
-    @Override
-    public String getCharacter()
-    {
-        return null;
-    }
+
 
     @Override
-    public void getInformation(FrameData arg0)
-    {
+    public int initialize( GameData gameData, boolean playerNumber ) {
+        this.playerNumber = playerNumber;
+        this.gameData = gameData;
 
-    }
+        this.key = new Key();
+        this.frameData = new FrameData();
+        this.commandCenter = new CommandCenter();
 
-    @Override
-    public int initialize(GameData var1, boolean var2)
-    {
+        this.myActions = new LinkedList<Action>();
+        this.oppActions = new LinkedList<Action>();
+
+        simulator = gameData.getSimulator();
+
+        /* create list of actions that can take place in the air */
+        actionAir =
+                new Action[] {Action.AIR_GUARD, Action.AIR_A, Action.AIR_B, Action.AIR_DA, Action.AIR_DB,
+                        Action.AIR_FA, Action.AIR_FB, Action.AIR_UA, Action.AIR_UB, Action.AIR_D_DF_FA,
+                        Action.AIR_D_DF_FB, Action.AIR_F_D_DFA, Action.AIR_F_D_DFB, Action.AIR_D_DB_BA,
+                        Action.AIR_D_DB_BB};
+
+        /* create list of actions that can take place on the ground */
+        actionGround =
+                new Action[] {Action.STAND_D_DB_BA, Action.BACK_STEP, Action.FORWARD_WALK, Action.DASH,
+                        Action.JUMP, Action.FOR_JUMP, Action.BACK_JUMP, Action.STAND_GUARD,
+                        Action.CROUCH_GUARD, Action.THROW_A, Action.THROW_B, Action.STAND_A, Action.STAND_B,
+                        Action.CROUCH_A, Action.CROUCH_B, Action.STAND_FA, Action.STAND_FB, Action.CROUCH_FA,
+                        Action.CROUCH_FB, Action.STAND_D_DF_FA, Action.STAND_D_DF_FB, Action.STAND_F_D_DFA,
+                        Action.STAND_F_D_DFB, Action.STAND_D_DB_BB};
+
+        /* Special Skill (costs most energy and deals most damage) */
+        spSkill = Action.STAND_D_DF_FC;
+
+        /* Conditional operator used for initialization. Is this.playerNumber = true ? do this : else this */
+        myMotion = this.playerNumber ? gameData.getPlayerOneMotion() : gameData.getPlayerTwoMotion();
+        oppMotion = this.playerNumber ? gameData.getPlayerTwoMotion() : gameData.getPlayerOneMotion();
+
         return 0;
     }
 
     @Override
-    public Key input()
-    {
-        return null;
+    public Key input() {
+        return key;
     }
 
-    @Override
-    public void processing()
-    {
+    /* AI determines ability to act */
+    public boolean canProcessing() { return !frameData.getEmptyFlag() && frameData.getRemainingTime() > 0; }
 
+    @Override
+    public void processing() {
+        /* AI determines ability to act */
+        if ( canProcessing() ) {
+
+            /* Checks input's state of a command */
+            if ( commandCenter.getskillFlag() ) {
+
+                /* Gets the current Key data */
+                key = commandCenter.getSkillKey();
+            }
+
+            else {
+                /* This method resets all keys to false, or not pressed */
+                key.empty();
+
+                /* Empties skillData and sets skillFlag to false */
+                commandCenter.skillCancel();
+
+                /* Perform in preparation of MCTS*/
+                mctsPrepare();
+                rootNode =
+                        new Node( simulatorAheadFrameData, null, myActions, oppActions, gameData, playerNumber, commandCenter );
+                rootNode.createNode();
+
+                /* Execute MCTS */
+                Action bestAction = rootNode.mcts();
+                if ( Fighting_AI.DEBUG_MODE ) {
+                    rootNode.printNode(rootNode);
+                }
+
+                /* Perform selected action chosen by MCTS */
+                commandCenter.commandCall(bestAction.name());
+            }
+        }
+    }
+
+    public void setMyAction() {
+        myActions.clear();
+
+        int energy = myCharacter.getEnergy();
+
+        if ( myCharacter.getState() == State.AIR ) {
+            for ( int i =0; i < actionAir.length; i++ ) {
+                if ( Math.abs( myMotion.elementAt( Action.valueOf( actionAir[i].name() ).ordinal() ).getAttackStartAddEnergy() ) <= energy ) {
+                    myActions.add( actionAir[i] );
+                }
+            }
+        }
+
+        else {
+            if ( Math.abs( myMotion.elementAt( Action.valueOf( spSkill.name() ).ordinal() ).getAttackStartAddEnergy() ) <= energy ) {
+                myActions.add( spSkill );
+            }
+
+            for ( int i = 0; i < actionGround.length; i++ ) {
+                if ( Math.abs( myMotion.elementAt( Action.valueOf( actionGround[i].name() ).ordinal() ).getAttackStartAddEnergy() ) <= energy ) {
+                    myActions.add( actionGround[i] );
+                }
+            }
+        }
+    }
+
+    public void setOppAction() {
+        oppActions.clear();
+
+        int energy = oppCharacter.getEnergy();
+
+        if ( oppCharacter.getState() == State.AIR ) {
+            for ( int i =0; i < actionAir.length; i++ ) {
+                if ( Math.abs( oppMotion.elementAt( Action.valueOf( actionAir[i].name() ).ordinal() ).getAttackStartAddEnergy() ) <= energy ) {
+                    myActions.add( actionAir[i] );
+                }
+            }
+        }
+
+        else {
+            if ( Math.abs( oppMotion.elementAt( Action.valueOf( spSkill.name() ).ordinal() ).getAttackStartAddEnergy() ) <= energy ) {
+                oppActions.add( spSkill );
+            }
+
+            for ( int i = 0; i < actionGround.length; i++ ) {
+                if ( Math.abs( oppMotion.elementAt( Action.valueOf( actionGround[i].name() ).ordinal() ).getAttackStartAddEnergy() ) <= energy ) {
+                    oppActions.add( actionGround[i] );
+                }
+            }
+        }
+    }
+
+    /* Get advance frame data to prepare MCTS*/
+    public  void mctsPrepare() {
+        simulatorAheadFrameData = simulator.simulate( frameData, playerNumber, null, null, FRAME_AHEAD );
+
+        myCharacter = playerNumber ? simulatorAheadFrameData.getP1() : simulatorAheadFrameData.getP2();
+        oppCharacter = playerNumber ? simulatorAheadFrameData.getP2() : simulatorAheadFrameData.getP1();
+
+        setMyAction();
+        setOppAction();
     }
 }
---
+++ src/Node.java
@@ -0,0 +1,313 @@
+/**
+ * Created by Jonathan on 5/3/2016.
+ */
+
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Random;
+
+import simulator.Simulator;
+import structs.CharacterData;
+import structs.FrameData;
+import structs.GameData;
+
+import commandcenter.CommandCenter;
+
+import enumerate.Action;
+
+public class Node {
+
+    /* Execution time of Upper Confidence Bound 1 applied to the tree (UCT) */
+    public static final int UCT_TIME = 165 * 100000;
+
+    /* Upper Confidence Bound 1 Value for constant C */
+    public static final double UCB_C = 3;
+
+    /*Depth of tree to search  */
+    public static final int UCT_TREE_DEPTH = 2;
+
+    /* Threshold to generate a node */
+    public static final int UCT_CREATE_NODE_THRESHOLD = 10;
+
+    /* Time to perform simulation */
+    public static final int SIMULATION_TIME = 60;
+
+    /* Use when using the random number */
+    private Random rnd;
+
+    /* Parent Node */
+    private Node parent;
+
+    /* Child Node */
+    private Node[] children;
+
+    /* Depth of Node */
+    private int depth;
+
+    /* Times a Node has been searched */
+    private int games;
+
+    /* Upper Confidence Bound Value */
+    private double ucb;
+
+    /* Evaluation Value */
+    private double score;
+
+    /* Actions AI can perform */
+    private LinkedList<Action> myActions;
+
+    /* All actions the opponent can perform */
+    private LinkedList<Action> oppActions;
+
+    /* Used for simulations */
+    private Simulator simulator;
+
+    /* Action(s) selected */
+    private LinkedList<Action> selectedMyActions;
+
+    /* Hp prior to simulation */
+    private int myOriginalHp;
+    private int oppOriginalHp;
+
+    private FrameData frameData;
+    private boolean playerNumber;
+    private CommandCenter commandCenter;
+    private GameData gameData;
+
+    private boolean isCreateNode;
+
+    Deque<Action> mAction;
+    Deque<Action> oppAction;
+
+    public Node( FrameData frameData, Node parent, LinkedList<Action> myActions, LinkedList<Action> oppActions, GameData gameData, boolean playerNumber, CommandCenter commandCenter) {
+        this.frameData = frameData;
+        this.parent = parent;
+        this.myActions = myActions;
+        this.oppActions = oppActions;
+        this.gameData = gameData;
+        this.simulator = new Simulator(gameData);
+        this.playerNumber = playerNumber;
+        this.commandCenter = commandCenter;
+
+        this.selectedMyActions = new LinkedList<Action>();
+
+        this.rnd = new Random();
+        this.mAction = new LinkedList<Action>();
+        this.oppAction = new LinkedList<Action>();
+
+        CharacterData myCharacter = playerNumber ? frameData.getP1() : frameData.getP2();
+        CharacterData oppCharacter = playerNumber ? frameData.getP2() : frameData.getP1();
+        myOriginalHp = myCharacter.getHp();
+        oppOriginalHp = oppCharacter.getHp();
+
+        if (this.parent != null) {
+            this.depth = this.parent.depth + 1;
+        }
+
+        else {
+            this.depth = 0;
+        }
+    }
+
+    public Node( FrameData frameData, Node parent, LinkedList<Action> myActions, LinkedList<Action> oppActions, GameData gameData, boolean playerNumber, CommandCenter commandCenter, LinkedList<Action> selectedMyActions ) {
+        this( frameData, parent, myActions, oppActions, gameData, playerNumber, commandCenter );
+
+        this.selectedMyActions = selectedMyActions;
+    }
+
+    /* Execute MCTS within UCT time frame */
+    public Action mcts() {
+        long start = System.nanoTime();
+        for ( ; System.nanoTime() - start <= UCT_TIME; ) {
+            uct();
+        }
+
+        return getBestVisitAction();
+    }
+
+    /* Run simulation and return evaluation value as results */
+    public double playout() {
+
+        mAction.clear();
+        oppAction.clear();
+
+        for ( int i = 0; i < selectedMyActions.size(); i++ ) {
+            mAction.add( selectedMyActions.get(i) );
+        }
+
+        for ( int i = 0; i < 5 - selectedMyActions.size(); i++ ) {
+            mAction.add( myActions.get( rnd.nextInt( myActions.size() ) ) );
+        }
+
+        for ( int i = 0; i < 5; i++ ) {
+            oppAction.add( oppActions.get(rnd.nextInt( oppActions.size() ) ) );
+        }
+
+        //Run Simulation
+        FrameData nFrameData = simulator.simulate( frameData, playerNumber, mAction, oppAction, SIMULATION_TIME );
+
+        return getScore( nFrameData );
+    }
+
+    /* Perform UCT and return evaluation value */
+    public double uct() {
+
+        Node selectedNode = null;
+        double bestUcb;
+
+        bestUcb = -99999;
+
+        for ( Node child : this.children ) {
+            if ( child.games == 0 ) {
+                child.ucb = 9999 + rnd.nextInt(50);
+            }
+
+            else {
+                child.ucb = getUcb( child.score / child.games, games, child.games );
+            }
+
+            if ( bestUcb < child.ucb ) {
+                selectedNode = child;
+                bestUcb = child.ucb;
+            }
+        }
+
+        double score = 0;
+
+        if ( selectedNode.games == 0 ) {
+            score = selectedNode.playout();
+        }
+
+        else {
+            if ( selectedNode.children == null ) {
+                if ( selectedNode.depth < UCT_TREE_DEPTH ) {
+                    if ( UCT_CREATE_NODE_THRESHOLD <= selectedNode.games ) {
+                        selectedNode.createNode();
+                        selectedNode.isCreateNode = true;
+                        score = selectedNode.uct();
+                    }
+
+                    else {
+                        score = selectedNode.playout();
+                    }
+                }
+
+                else {
+                    score = selectedNode.playout();
+                }
+            }
+
+            else {
+                if ( selectedNode.depth < UCT_TREE_DEPTH ) {
+                    score = selectedNode.uct();
+                }
+
+                else {
+                    selectedNode.playout();
+                }
+            }
+        }
+
+        selectedNode.games++;
+        selectedNode.score += score;
+
+        if ( depth == 0 ) {
+            games++;
+        }
+
+        return score;
+    }
+
+    /* Generate Node */
+    public void createNode() {
+
+        this.children = new Node[myActions.size()];
+
+        for ( int i = 0; i < children.length; i++ ) {
+
+            LinkedList<Action> my = new LinkedList<Action>();
+            for ( Action act : selectedMyActions ) {
+                my.add(act);
+            }
+
+            my.add(myActions.get(i));
+
+            children[i] = new Node( frameData, this, myActions, oppActions, gameData, playerNumber, commandCenter, my );
+        }
+    }
+
+    /* Return Evaluation Value using frame data */
+    public int getScore( FrameData fd ) {
+        return playerNumber ? ( fd.getP1().hp - myOriginalHp ) - ( fd.getP2().hp - oppOriginalHp ) : ( fd.getP2().hp - myOriginalHp ) - ( fd.getP1().hp - oppOriginalHp );
+    }
+
+    /* Return Action of Node most visited */
+    public Action getBestVisitAction() {
+
+        int selected = -1;
+        double bestGames = -9999;
+
+        for ( int i = 0; i < children.length; i++ ) {
+
+            if (Fighting_AI.DEBUG_MODE) {
+                System.out.println("Evaluation Value:" + children[i].score / children[i].games + ", Number of Trials:" + children[i].games + ", UCB:" + children[i].ucb + ", Action:" + myActions.get(i) );
+            }
+
+            if ( bestGames < children[i].games ) {
+                bestGames = children[i].games;
+                selected = i;
+            }
+        }
+
+        if (Fighting_AI.DEBUG_MODE) {
+            System.out.println( myActions.get( selected ) + ", Number of Trials:" + games );
+            System.out.println( "" );
+        }
+
+        return this.myActions.get( selected );
+    }
+
+    /* Return Node of Action with highest score */
+    public Action getBestScoreAction() {
+
+        int selected = -1;
+        double bestScore = -9999;
+
+        for ( int i = 0; i < children.length; i++ ) {
+
+            System.out.println( "Evaluation Value:" + children[i].score / children[i].games + ", Number of Trials:" + children[i].games + ", UCB:" + children[i].ucb + ",Action:" + myActions.get(i) );
+
+            double meanScore = children[i].score / children[i].games;
+
+            if ( bestScore < meanScore ) {
+                bestScore = meanScore;
+                selected = i;
+            }
+        }
+
+        System.out.println( myActions.get( selected ) + ", Number of Trials:" + games );
+        System.out.println( "" );
+
+        return this.myActions.get( selected );
+    }
+
+    /* Return Upper Confidence Bound 1 Value using: Evaluation Value(score), Number of All Simulations(n), Number of attempts concerning action of simulation(ni) */
+    public double getUcb( double score, int n, int ni ) {
+        return score + UCB_C * Math.sqrt( ( 2 * Math.log(n) ) / ni );
+    }
+
+    public void printNode( Node node ) {
+        System.out.println( "Number of Trials:" + node.games );
+        for (int i = 0; i < node.children.length; i++) {
+            System.out.println( i + ", Frequency of Recurrence:" + node.children[i].games + ", Tree Depth Location:" + node.children[i].depth + ", Score:" + node.children[i].score / node.children[i].games + ", UCB:" + node.children[i].ucb);
+        }
+
+        System.out.println( "" );
+
+        for ( int i = 0; i < node.children.length; i++ ) {
+            if ( node.children[i].isCreateNode ) {
+                printNode( node.children[i] );
+            }
+        }
+    }
+}
